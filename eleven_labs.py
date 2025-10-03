bd63846f1c37cb62a7dda85429563fc0e90c3fbd
JackMorin25 <jackmorin757@gmail.com>
2025-10-03 10:02:28 -0500

    set api in file from env

--- eleven_labs.py
+++ eleven_labs.py
@@ -1,3 +1,16 @@
+from elevenlabs import generate, stream, set_api_key, voices, play, save
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+set_api_key(os.getenv("11LABS_API_KEY"))
+
+
 class ElevenLabsManager:
     def __init__(self):
+        all_voices = voices()
+        print(f"using these voices {all_voices}")
+
+    def play_text_to_speech(self):
         pass
