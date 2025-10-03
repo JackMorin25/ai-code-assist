3851b959554298611dbdc2e96752050885b465c5
JackMorin25 <jackmorin757@gmail.com>
2025-10-03 10:48:57 -0500

    moved wehre we set api_key

--- eleven_labs.py
+++ eleven_labs.py
@@ -5,14 +5,16 @@ import os
 
 
 load_dotenv()
-elevenlabs = ElevenLabs(
-    api_key=os.getenv("11LABS_API_KEY")
-)
 
 
 class ElevenLabsManager:
+    def __init__(self):
+        self.elevenlabs = ElevenLabs(
+            api_key=os.getenv("11LABS_API_KEY")
+        )
+
     def play_text_to_speech(self, input_text, voice="zEc80FtpUcM40u6WiffU"):
-        audio = elevenlabs.text_to_speech.convert(
+        audio = self.elevenlabs.text_to_speech.convert(
             text=input_text,
             voice_id=voice,
             model_id="eleven_multilingual_v2",
